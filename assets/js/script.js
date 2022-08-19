bd82852fc47b3a481e95c8d297cc9aeca7c55a89
Júnior Baum <juniorsbaum@hotmail.com>
2022-08-18 23:05:05 -0300

    feat: header e profile

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,11 @@ const navToggleBtn = document.querySelector("[data-menu-toglebtn]");
 navToggleBtn.addEventListener("click", function () {
     header.classList.toggle("active");
 });
+
+const menuBtn = document.querySelectorAll("[data-menu-btn]");
+
+for (let i = 0; i < menuBtn.length; i++) {
+  menuBtn[i].addEventListener("click", function () {
+    this.nextElementSibling.classList.toggle("active");
+  });
+}
